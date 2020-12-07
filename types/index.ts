f89b9c1af3254d0486758eceb48f36ee48a2e8e8
ysk_univ <ysk.univ.1007@gmail.com>
2020-12-07 21:20:54 +0900

    [types/index.ts] remove export of types on lineWebhookEventObjects

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,2 @@
-export {
-  LineWebhookEventObject,
-  LineEvent,
-  EventSource,
-  EventSourceUser,
-  MessageEvent,
-  MessageEventMessage,
-  MessageEventTextMessage
-} from './lineWebhookEventObjects'
-
 export type ReplyAction = (text: string) => Promise<void>
 export type MessageHandler = (message: string, reply: ReplyAction) => Promise<void>
